11d3e482e6f0ba508554cbd11db91b85efafea8c
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-05-23 13:33:05 +0600

    added navbar and footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css'
-import {
-  RouterProvider,
-} from "react-router-dom";
-import router from './Routes/Routes';
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <div className="max-w-screen-2xl	mx-auto">
+      <RouterProvider router={router} />
+    </div>
+  </React.StrictMode>
+);
