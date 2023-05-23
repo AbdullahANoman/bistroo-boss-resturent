446289ebe6fef4232837a06c716cc3d931062f62
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-05-24 01:22:50 +0600

    added banner and category and banner with carousel and category swipper

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
 
 
 const Home = () => {
     return (
         <div>
-            <p>This is Home</p>
+            <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };
